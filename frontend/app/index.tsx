1d4a5af8396a05b459073ddc1a90b58d5498e3fd
Tarun kataria <tarunsoftsol@gmail.com>
2025-09-29 21:26:43 +0530

    react-native-setup

---
+++ frontend/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="bg-red-500" >
+
+      <Text>native wind setup this is code code red  </Text>
+
+    </SafeAreaView>
+  );
+}
