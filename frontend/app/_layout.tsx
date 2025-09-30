bdb0bedfcc21e3471ac1078da11ce0d109fd42cb
Tarun kataria <tarunsoftsol@gmail.com>
2025-09-30 19:33:38 +0530

    nativewind setup complete

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-
+import '../global.css'
 export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="index" options={{ headerShown: false }} />
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,12 +1,10 @@
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import '../global.css'
 export default function Index() {
   return (
-    <SafeAreaView className="bg-red-500" >
-
-      <Text>native wind setup this is code code red  </Text>
-
+    <SafeAreaView className=" bg-red-500 justify-center items-center ">
+      <Text>hello world</Text>
     </SafeAreaView>
   );
 }
